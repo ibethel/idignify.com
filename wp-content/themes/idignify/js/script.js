fe352aa73b4b5e7a374986a8beb7645ee4277595
Jesse Kade <jessekade@gmail.com>
2012-05-16 09:37:33 -0700

    cleaned up css and swapped out bad images

--- wp-content/themes/idignify/js/script.js
+++ wp-content/themes/idignify/js/script.js
@@ -68,3 +68,36 @@ jQuery(document).ready(function(){
 	}).resize();
 	
 });
+
+jQuery(document).ready(function(){
+	$(function() {
+		var input = document.createElement("input");
+	    if(('placeholder' in input)==false) { 
+			$('[placeholder]').focus(function() {
+				var i = $(this);
+				if(i.val() == i.attr('placeholder')) {
+					i.val('').removeClass('placeholder');
+					if(i.hasClass('password')) {
+						i.removeClass('password');
+						this.type='password';
+					}			
+				}
+			}).blur(function() {
+				var i = $(this);	
+				if(i.val() == '' || i.val() == i.attr('placeholder')) {
+					if(this.type=='password') {
+						i.addClass('password');
+						this.type='text';
+					}
+					i.addClass('placeholder').val(i.attr('placeholder'));
+				}
+			}).blur().parents('form').submit(function() {
+				$(this).find('[placeholder]').each(function() {
+					var i = $(this);
+					if(i.val() == i.attr('placeholder'))
+						i.val('');
+				})
+			});
+		}
+	});
+});
