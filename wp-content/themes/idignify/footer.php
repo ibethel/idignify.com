f1e4048ad12cfaa69383684d8710972b2dd9c8a2
Jesse Kade <jessekade@gmail.com>
2012-05-30 13:23:37 -0700

    finished tablet layout and almost done with phone layout except for home page

--- wp-content/themes/idignify/footer.php
+++ wp-content/themes/idignify/footer.php
@@ -9,13 +9,13 @@
 			</p>
 		</div>
 	</footer> 
-<!--
+
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 <script src="<?php bloginfo('template_directory'); ?>/js/bootstrap-collapse.js" type="text/javascript" charset="utf-8"></script>
 <script src="<?php bloginfo('template_directory'); ?>/js/scrollspy.js" type="text/javascript" charset="utf-8"></script>
 <script src="<?php bloginfo('template_directory'); ?>/js/plugins.js" type="text/javascript" charset="utf-8"></script>
 <script src="<?php bloginfo('template_directory'); ?>/js/script.js" type="text/javascript" charset="utf-8"></script>
--->
+
 <?php wp_footer(); ?>
 </body>
 </html>
