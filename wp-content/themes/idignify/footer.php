8d9c3d636c83e96159942875e98d137821378ead
Jesse Kade <jessekade@gmail.com>
2012-05-30 10:17:07 -0700

    fixing for android

--- wp-content/themes/idignify/footer.php
+++ wp-content/themes/idignify/footer.php
@@ -11,8 +11,7 @@
 	</footer> 
 	
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>/js/bootstrap-collapse.js" type="text/javascript" charset="utf-8"></script>
-<script src="<?php bloginfo('template_directory'); ?>/js/scrollspy.js" type="text/javascript" charset="utf-8"></script>
+
 <script src="<?php bloginfo('template_directory'); ?>/js/plugins.js" type="text/javascript" charset="utf-8"></script>
 <script src="<?php bloginfo('template_directory'); ?>/js/script.js" type="text/javascript" charset="utf-8"></script>
 
