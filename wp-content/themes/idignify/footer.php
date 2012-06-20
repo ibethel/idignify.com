85738d3aeb05aea06012506cfd6469fc62d9f7b9
Jesse Kade <jessekade@gmail.com>
2012-06-19 21:38:56 -0700

    trying to fix android footer bottom margin

--- wp-content/themes/idignify/footer.php
+++ wp-content/themes/idignify/footer.php
@@ -1,6 +1,6 @@
 	<footer>
 		<div class="container">
-			<p><span class="legal">&copy;<?php echo date('Y'); ?> I.Dignify&nbsp;</span>Let's <strong>Free</strong> Someone Together
+			<p style="margin: 0;"><span class="legal">&copy;<?php echo date('Y'); ?> I.Dignify&nbsp;</span>Let's <strong>Free</strong> Someone Together
 				<span>
 					<a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/posterous.png" width="23" height="23" alt="Posterous"></a>
 					<a href="http://www.facebook.com/idignify"><img src="<?php bloginfo('template_directory'); ?>/images/facebook.png" width="8" height="16" alt="Facebook"></a>
@@ -11,7 +11,6 @@
 	</footer> 
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>/js/bootstrap-collapse.js" type="text/javascript" charset="utf-8"></script>
 <script src="<?php bloginfo('template_directory'); ?>/js/scrollspy.js" type="text/javascript" charset="utf-8"></script>
 <script src="<?php bloginfo('template_directory'); ?>/js/plugins.js" type="text/javascript" charset="utf-8"></script>
 <script src="<?php bloginfo('template_directory'); ?>/js/script.js" type="text/javascript" charset="utf-8"></script>
